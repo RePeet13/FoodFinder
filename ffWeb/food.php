65b131b36cb5959a9748c91cd0efdb901265a0f6
Taylor Peet <repeet13@gmail.com>
2013-10-18 15:38:30 -0400

    added first crack at data retrieval, should be able to lest, get specific and add

--- ffWeb/food.php
+++ ffWeb/food.php
@@ -1,48 +1,31 @@
 <?php
 	include 'db_helper.php';
-	
-	function listEvents() {
-		$dbQuery = sprintf("SELECT `EVENT_ID`, `DATE`, `ORG_ID`, `TITLE`, `FOURSQUARE`, `ADDRESS`, `START_TIME`, `END_TIME`, `APPROVED_DATE`, `SUMMARY`, `TYPE`, `SPECIAL_NOTES`, `ALCOHOL`, `CREATED_DATE`,`DATE_CHANGED` FROM `EVENTS`");
+  
+	function listfood() {
+		$dbQuery = sprintf("SELECT `food_id`, `date`, `gtid`, `title`, `location`, `description`, `pic_url`, `upvotes`, `lat_long`, `date_modified` FROM `food`");
+		
 		$result = getDBResultsArray($dbQuery);
+		
 		header("Content-type: application/json");
 		echo json_encode($result);
 	}
-	
+  
 	function getEvent($id) {
-		$dbQuery = sprintf("SELECT `EVENT_ID`, `DATE`, `ORG_ID`, `TITLE`, `FOURSQUARE`, `ADDRESS`, `START_TIME`, `END_TIME`, `APPROVED_DATE`, `SUMMARY`, `TYPE`, `SPECIAL_NOTES`, `ALCOHOL`, `CREATED_DATE`,`DATE_CHANGED` FROM `EVENTS` WHERE `EVENT_ID` =  '%s'", mysql_real_escape_string($id));
+		$dbQuery = sprintf("SELECT `food_id`, `date`, `gtid`, `title`, `location`, `description`, `pic_url`, `upvotes`, `lat_long`, `date_modified` FROM `food` WHERE `food_id` =  '%s'", mysql_real_escape_string($id));
+		// echo $dbQuery;
 		$result=getDBResultRecord($dbQuery);
-		header("Content-type: application/json");
-		echo json_encode($result);
-	}
-	
-	function getThisWeek() {
-		$dbQuery = sprintf("SELECT `EVENT_ID`, `DATE`, `ORG_ID`, `TITLE`, `FOURSQUARE`, `ADDRESS`, `START_TIME`, `END_TIME`, `APPROVED_DATE`, `SUMMARY`, `TYPE`, `SPECIAL_NOTES`, `ALCOHOL`, `CREATED_DATE`,`DATE_CHANGED` FROM `EVENTS` WHERE `DATE` > CURDATE() AND `DATE` < CURDATE() + 7");
-		$result=getDBResultRecord($dbQuery);
-		header("Content-type: application/json");
-		echo json_encode($result);
-	}
-
-/*	
-	function addEvent($event) {
-		$dbQuery = sprintf("INSERT INTO events (event) VALUES ('%s')",
-			mysql_real_escape_string($events));
-	
-		$result = getDBResultInserted($dbQuery,'personId');
 		
 		header("Content-type: application/json");
 		echo json_encode($result);
 	}
 	
-	function updateEvent($id,$event) {
-		$dbQuery = sprintf("UPDATE events SET event = '%s' WHERE id = '%s'",
-			mysql_real_escape_string($events),
-			mysql_real_escape_string($id));
-		
-		$result = getDBResultAffected($dbQuery);
+	function addFood($foodDate, $foodTitle, $foodLocation, $foodDescription, $foodPicUrl, $foodLatLong) {
+		// TODO only allow if uid is not empty
+		$dbQuery = sprintf("INSERT INTO food (`date`, `gtid`, `title`, `location`, `description`, `pic_url`, `lat_long`) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')", mysql_real_escape_string($foodDate), $_USER['uid'], mysql_real_escape_string($foodTitle), mysql_real_escape_string($foodLocation), mysql_real_escape_string($foodDescription), mysql_real_escape_string($foodPicUrl), mysql_real_escape_string($foodLatLong));
+		echo "Query " . $dbQuery . "</br>";
+		$result = getDBResultInserted($dbQuery,'food_id');
 		
 		header("Content-type: application/json");
-		echo json_encode($result);
+		echo json_encode($result) . "</br>";
 	}
-*/
-
 ?>
